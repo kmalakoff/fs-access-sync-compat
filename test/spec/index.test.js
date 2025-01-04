55c59bdc007544e1615fb4b8118b4008945bf812
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 19:51:54 -0800

    Migrate to TypeScript and Biome toolchain

--- test/spec/index.test.js
+++ test/spec/index.test.js
@@ -1,24 +1,23 @@
-var assert = require('assert');
+const assert = require('assert');
 
-var accessSync = require('../..');
+const accessSync = require('fs-access-sync-compat');
 
-describe('fs-access-sync-compat', function () {
-  it('exists', function (done) {
-    assert.doesNotThrow(function () {
+describe('fs-access-sync-compat', () => {
+  it('exists', (done) => {
+    assert.doesNotThrow(() => {
       accessSync(__filename);
     });
     done();
   });
 
-  it('does not exist', function (done) {
+  it('does not exist', (done) => {
     try {
-      // eslint-disable-next-line no-path-concat
-      accessSync(__filename + 'junk');
+      accessSync(`${__filename}junk`);
       assert.ok(false);
     } catch (err) {
       assert.equal(err.message.indexOf('ENOENT'), 0);
       assert.equal(err.code, 'ENOENT');
-      assert.ok(~[-2, -4058].indexOf(err.errno));
+      assert.equal(err.errno, -2);
       done();
     }
   });
